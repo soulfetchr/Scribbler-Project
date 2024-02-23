85eefb724aa58078801fdfac29c8ca7f08ad1001
soulfetchr <yad.vivek2001@gmail.com>
2024-02-23 15:24:22 +0530

    home page completed

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+let signUpModal = document.getElementById("signupModel");
+let signInModel = document.getElementById("signinModel");
+let createPostModel = document.getElementById("creatPostModel");
+let signUpButton = document.getElementById("signupbutton");
+let signInButton = document.getElementById("signinbutton");
+let createPostButton = document.getElementById("CreatePostButton");
+let closebutton = document.getElementsByClassName("close");
+let openSignUPLink = document.getElementById("OpenSignUp");
+let allPosts = document.getElementById("postsLists");
+
+allPosts.onclick = () => {
+    window.location.href = "postslist.html";
+}
+
+signUpButton.onclick = () => {
+    signUpModal.style.display = "block";
+}
+
+signInButton.onclick = () => {
+    signInModel.style.display = "block";
+}
+
+createPostButton.onclick = () => {
+    createPostModel.style.display = "block";
+}
+
+openSignUPLink.onclick = function() {
+    signUpModal.style.display = "block";
+    signInModel.style.display = "none";
+}
+
+for(let i=0; i <closebutton.length; i++){
+    closebutton[i].onclick = ()=>{
+    signUpModal.style.display = "none";
+    signInModel.style.display = "none";
+    createPostModel.style.display = "none";
+}
+}
